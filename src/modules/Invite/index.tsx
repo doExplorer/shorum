9568b3062d900eabc47c6b69e3810b2d9345652c
Kingller Tang <kingller@126.com>
2022-05-30 02:38:38 +0800

    invite页面修改

--- src/modules/Invite/index.tsx
+++ src/modules/Invite/index.tsx
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react';
 import hashHistory from 'hash-history';
 import { useParams } from 'react-router-dom';
 import useFollowContract from 'contract/useFollowContract';
-import { Form, Checkbox, Button, Input } from 'antd';
+import { Form, Checkbox, Button, Input, Select } from 'antd';
 import { useWallet } from 'use-wallet';
 import Web3Utils from 'web3-utils';
 import _ from 'lodash';
@@ -18,20 +18,22 @@ import './style.less';
 
 import store from './store';
 
+const { Option } = Select;
+
 const Invite = observer(function () {
     const { options } = store;
     const { profileId } = useParams<{ profileId?: string }>();
     const followContract = useFollowContract();
     const wallet = useWallet();
     const { account } = wallet;
+    const [form] = Form.useForm();
 
     useEffect(() => {
         store.loadData(account);
     }, [account]);
 
-    const onFinish = async (values: { inviteValue: string[]; addresses: string[] }) => {
-        console.log('Success:', values);
-        store.onInvite(values, async (inviteList: string[]) => {
+    const onInvite = async () => {
+        store.onInvite(async (inviteList: string[]) => {
             await followContract.invite(inviteList, profileId);
             console.log('ready to jump');
             hashHistory.push(`/room/${account}`);
@@ -39,25 +41,19 @@ const Invite = observer(function () {
         });
     };
 
-    const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
-    };
-
-    const handleChange = (e: React.ChangeEvent) => {
-        const { value } = e.target as HTMLInputElement;
-        store.onInputChange(value);
+    const handleAddAddress = (value: string) => {
+        store.onAddAdress(value);
     };
 
     return (
         <ModuleContainer className="invite-page common-container" title="Step3">
             <Form
+                form={form}
                 className="invite-form"
                 name="invite"
                 layout="vertical"
                 colon={false}
                 initialValues={{}}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
                 autoComplete="off">
                 <div className="invite-form-body">
                     <div className="powered-by">
@@ -65,73 +61,83 @@ const Invite = observer(function () {
                         <img className="knn3-logo" src={knn3Logo} alt="" />
                         <img className="rss3-logo" src={rss3Logo} alt="" />
                     </div>
-                    <Form.Item label="Do you want to invite some backers?" name="inviteValue">
-                        <Checkbox.Group className="invite-list" options={options} />
-                    </Form.Item>
-                    <Form.List name="addresses" initialValue={['']}>
-                        {(
-                            fields: { name: number; key: number }[],
+                    <div className="title">Do you want to invite some backers?</div>
+                    <div className="ant-row select-box">
+                        <label>Source</label>
+                        <Select value={store.source} style={{ width: 120 }} onChange={store.handleSourceChange}>
+                            <Option value="knn3">knn3</Option>
+                            <Option value="rss3">rss3</Option>
+                        </Select>
+                        <If condition={store.source !== 'rss3'}>
+                            <label>Algo</label>
+                            <Select value={store.algo} style={{ width: 120 }} onChange={store.handleAlgoChange}>
+                                <Option value="OVERLAP">OVERLAP</Option>
+                                <Option value="JACCARD">JACCARD</Option>
+                            </Select>
+                        </If>
+                    </div>
+                    <Form.Item
+                        name="address"
+                        validateTrigger={['onChange', 'onBlur']}
+                        rules={[
                             {
-                                add,
-                                remove,
-                            }: {
-                                add: (defaultValue?: string, insertIndex?: number) => void;
-                                remove: (index: number | number[]) => void;
-                            }
-                        ) => (
-                            <>
-                                {fields.map((field, index) => (
-                                    <Form.Item label="" required={false} key={field.key}>
-                                        <Form.Item
-                                            {...field}
-                                            validateTrigger={['onChange', 'onBlur']}
-                                            rules={[
-                                                {
-                                                    validator: (rule: RuleObject, value: string) => {
-                                                        const trimValue = _.trim(value);
-                                                        if (!trimValue) {
-                                                            return Promise.resolve();
-                                                        }
-                                                        if (!Web3Utils.isAddress(trimValue, utils.getChain())) {
-                                                            return Promise.reject();
-                                                        }
-                                                        return Promise.resolve();
-                                                    },
-                                                    message: 'Please input an available address or delete this field',
-                                                },
-                                            ]}
-                                            noStyle>
-                                            <Input
-                                                placeholder="invite address"
-                                                style={{ width: '70%' }}
-                                                onPressEnter={(e: React.KeyboardEvent) => {
-                                                    e.preventDefault();
+                                validator: (rule: RuleObject, value: string) => {
+                                    const trimValue = _.trim(value);
+                                    if (!trimValue) {
+                                        return Promise.resolve();
+                                    }
+                                    if (!Web3Utils.isAddress(trimValue, utils.getChain())) {
+                                        return Promise.reject();
+                                    }
+                                    return Promise.resolve();
+                                },
+                                message: 'Please input an available address',
+                            },
+                            {
+                                validator: (rule: RuleObject, value: string) => {
+                                    const trimValue = _.trim(value);
+                                    if (!trimValue) {
+                                        return Promise.resolve();
+                                    }
+                                    if (store.options.find((o) => o.value === trimValue)) {
+                                        return Promise.reject();
+                                    }
+                                    return Promise.resolve();
+                                },
+                                message: 'This address has been added!',
+                            },
+                        ]}>
+                        <Input
+                            placeholder="invite address"
+                            style={{ width: '70%' }}
+                            onPressEnter={(e: React.KeyboardEvent) => {
+                                e.preventDefault();
 
-                                                    const trimValue = _.trim((e.target as HTMLInputElement).value);
-                                                    if (trimValue) {
-                                                        if (!Web3Utils.isAddress(trimValue, utils.getChain())) {
-                                                            return;
-                                                        }
-                                                    }
-                                                    add('', index + 1);
-                                                }}
-                                                onChange={handleChange}
-                                            />
-                                        </Form.Item>
-                                        {fields.length > 1 ? (
-                                            <MinusCircleOutlined
-                                                className="dynamic-delete-button"
-                                                onClick={() => remove(field.name)}
-                                            />
-                                        ) : null}
-                                    </Form.Item>
-                                ))}
-                            </>
-                        )}
-                    </Form.List>
+                                const trimValue = _.trim((e.target as HTMLInputElement).value);
+                                if (trimValue) {
+                                    if (!Web3Utils.isAddress(trimValue, utils.getChain())) {
+                                        return;
+                                    }
+                                    if (store.options.find((o) => o.value === trimValue)) {
+                                        return;
+                                    }
+                                    handleAddAddress(trimValue);
+                                }
+                                form.setFieldsValue({
+                                    address: '',
+                                });
+                            }}
+                        />
+                    </Form.Item>
+                    <Checkbox.Group
+                        className="invite-list"
+                        options={Array.from(options)}
+                        value={store.checkedList}
+                        onChange={store.onCheckedChange}
+                    />
                 </div>
                 <div className="invite-form-footer">
-                    <Button type="primary" htmlType="submit" size="large">
+                    <Button type="primary" size="large" onClick={onInvite}>
                         Invite
                     </Button>
                 </div>

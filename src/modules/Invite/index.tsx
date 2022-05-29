60a81ee7ddae2cc75c59fa5c3ac6201f70082016
Kingller Tang <kingller@126.com>
2022-05-29 23:45:54 +0800

    nft owner修改

--- src/modules/Invite/index.tsx
+++ src/modules/Invite/index.tsx
@@ -31,7 +31,7 @@ const Invite = observer(function () {
         console.log('Success:', values);
         store.onInvite(values, async (inviteList: string[]) => {
             await followContract.invite(inviteList, profileId);
-            console.log('ready to jump')
+            console.log('ready to jump');
             hashHistory.push(`/room/${account}`);
             // return result;
         });
